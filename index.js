293761800821840df3c3b71bdfa818e55d1ca45f
Pedro Fernandez <pfernandezp91@gmail.com>
2021-05-26 12:37:11 -0500

    funciona

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
@@ -22,5 +23,8 @@ io.on("connection", function(socket){
 	console.log("listen in 3000");
 });*/
 
-var port = http.listen(process.env.PORT || 3000);
-app.listen(port);
+http.listen(port, function(){
+    console.log('listening on :' + port);
+});
+
+//app.listen(port);
